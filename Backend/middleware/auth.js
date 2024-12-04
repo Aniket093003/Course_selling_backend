6b3ddf3a08e707a771c14b55aefef49f602ebf4e
Aniket093003 <aniketkainth98105@gmail.com>
2024-12-05 00:23:40 +0530

    auth middleware

---
+++ Backend/middleware/auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import User from '../model/user.model';
+
+
+
+const authJWT = async (req, res, next) => {
+    const token = req.header('Authorization')?.split(' ')[1]; // Bearer <token>
+    if (!token) return res.status(401).json({ message: 'Unauthorized' });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id);
+        if (!req.user) throw new Error('User not found');
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid or expired token' });
+    }
+};
+
+const authRoles = (...roles) => (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+        return res.status(403).json({ message: 'Access Denied' });
+    }
+    next();
+};
+
+
+export default {authJWT, authRoles}
